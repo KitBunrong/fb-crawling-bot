739eae46e0b6ee20e31c35727d6fb1df320d9ab0
garyswartz18@gmail.com <garyswartz18@gmail.com>
2021-01-18 21:49:35 +0700

    fn v1.0

--- scraping.py
+++ scraping.py
@@ -1,39 +1,71 @@
-import time
 import re
+import time
 import json
+import random
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.chrome.options import Options 
-import sys
 
 login_form_url = "https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=100"
+
+def sent_message():
+    try: 
+        text = driver.find_element_by_xpath('//*[@id="composerInput"]')
+        text.send_keys('Hii :)')
+        print("Message inputted...")
+        sent = driver.find_element_by_xpath('//*[@id="composer_form"]/table/tbody/tr/td[2]/input')
+        sent.click()
+        print("Message sent clicked...")
+    except Exception:
+        text = driver.find_element_by_xpath('//*[@id="composer_form"]/div[2]/table/tbody/tr/td[1]/textarea')
+        text.send_keys('Hii :)')
+        print("Message inputted...")
+        sent = driver.find_element_by_xpath('//*[@id="composer_form"]/div[2]/table/tbody/tr/td[2]/input')
+        sent.click()
+        print("Message button clicked...")
 
 def add_fri_sent_mess(profile_url):
     """ Automatic add frind to and get sent message.
     """
     driver.get(profile_url)
+
     try: 
-        add = driver.find_element_by_xpath("//table/tbody/tr/td/a[.='Add Friend']")
-        add.click()
-        bname = driver.find_element_by_xpath("//*[@id='root']/div[1]/div[1]/div[1]/div[1]/span/strong")
-        print("'Sent Friend Reqest to :", bname.get_attribute('innerHTML'), " , Waiting for Comfirmation...")
+        # add = driver.find_element_by_xpath("//table/tbody/tr/td/a[.='Add Friend']")
+        add = driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[1]/div[3]/table/tbody/tr/td[1]/a')
+        print(add.get_attribute('innerHTML'))
+        if(add.get_attribute('innerHTML') == 'Add Friend'):
+            add.click()
+            
+            bname = driver.find_element_by_xpath("//*[@id='root']/div[1]/div[1]/div[1]/div[1]/span/strong")
+            print("'Sent Friend Reqest to :", bname.get_attribute('innerHTML'), " , Waiting for Comfirmation...")
+
+            msg = driver.find_element_by_xpath('//*[@id="root"]/div[1]/div[1]/div[1]/div[1]/div/table/tbody/tr/td[2]/a')
+            msg.click()
+            print("Message button clicked...")
+            sent_message()
+
+        elif(add.get_attribute('innerHTML') == 'Message'):
+            name = driver.find_element_by_xpath('//*[@id="m-timeline-cover-section"]/div[2]/div[1]/span/div/span/strong')
+            print('You already friend with :', name.get_attribute('innerHTML'))
+
     except Exception:
         try: 
-            name = driver.find_element_by_xpath("//*[@id='m-timeline-cover-section']/div[2]/div[1]/span/div/span/strong")
-            print("You already friend with :", name.get_attribute("innerHTML"))
-        except Exception:
             name = driver.find_element_by_xpath("//*[@id='root']/div[1]/div[1]/div[2]/div/span/div/span/strong")
-            print("Waiting Confirmation from :", name.get_attribute("innerHTML"))
+            print("Waiting Confirmation from :", name.get_attribute("innerHTML"))            
+        except Exception:
+            print("There's Something Went Wrong...")   
 
-    ## ? When should message sent to:
-    ## ? 1. At the same time we sent friend request
-    ## ? 2. When we got friend comfirmation
+    # ? When should message sent to:
+    # ? 1. At the same time we sent friend request
+    # ? 2. When we got friend comfirmation
     # text = driver.find_element_by_xpath("//table/tbody/tr/td/textarea")
     # sent.click()
     # sent = driver.find_element_by_xpath("//table/tbody/tr/td[2]/a[.='Message']")
     # print("Message button clicked")
     # text.send_keys("Hello Hi bybybye!!")
     # print("text")
+
 
 def json_to_obj(filename):
     """ Extract data from JSON file and save it on Python object 
@@ -44,12 +76,31 @@ def json_to_obj(filename):
         obj = json.loads(json_file.read())
     return obj
 
+def dataframe_to_obj():
+    """ Extract data from csv file and save it to Python object 
+    """
+    print("*** : dataframe_to_obj ***")
+    list_url = []
+    filename = input("Enter filename [e.g., sample.csv] : ")
+    try: 
+        profile_urls = pd.read_csv(filename)
+    except Exception:
+        print("Recheck your filename...")   
+        return
+    cname =profile_urls.columns[0]
+    for i in range(len(profile_urls)):
+        ls = profile_urls.loc[i, cname]
+        list_url.append("https://mbasic" + ls[11:])
+        # print(list_url.append(profile_urls.loc[i, cname]))
+    return list_url
+
 if __name__ == "__main__":
     ### Extracts credentials for the login and all of the profiles URL to scrape
     credentials = json_to_obj('credentials.json')
-    profile_urls = json_to_obj('profiles_urls.json')
+    profile_urls = dataframe_to_obj()
+    # profile_urls = json_to_obj('profiles_urls.json')
 
-    ### To block the notifications
+    ### Block the notifications
     options = webdriver.ChromeOptions()
     prefs = {"profile.default_content_setting_values.notifications" : 2}
     options.add_experimental_option("prefs", prefs)
@@ -76,6 +127,7 @@ if __name__ == "__main__":
 
     for profile_url in profile_urls:
         ### Auto add friend and sent "xxx" message to
+        sleep = random.randint(5,10)
         add_fri_sent_mess(profile_url)
-        time.sleep(2)
-
+        print("[***] Sleep for :", sleep)
+        time.sleep(sleep)
